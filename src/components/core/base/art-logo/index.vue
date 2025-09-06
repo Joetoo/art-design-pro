497d647475ed1d7f838b8377343b830e64e09b17
Joet桀 <zoexml@gmail.com>
2025-09-06 21:41:35 +0800

    style: 💄 文件格式化

--- src/components/core/base/art-logo/index.vue
+++ src/components/core/base/art-logo/index.vue
@@ -1,34 +1,34 @@
 <!-- 系统logo -->
+<script setup lang="ts">
+defineOptions({ name: 'ArtLogo' })
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 36,
+})
+
+interface Props {
+  /** logo 大小 */
+  size?: number | string
+}
+
+const logoStyle = computed(() => ({ width: `${props.size}px` }))
+</script>
+
 <template>
   <div class="art-logo">
-    <img :style="logoStyle" src="@imgs/common/logo.webp" alt="logo" />
+    <img :style="logoStyle" src="@imgs/common/logo.webp" alt="logo">
   </div>
 </template>
 
-<script setup lang="ts">
-  defineOptions({ name: 'ArtLogo' })
-
-  interface Props {
-    /** logo 大小 */
-    size?: number | string
-  }
-
-  const props = withDefaults(defineProps<Props>(), {
-    size: 36
-  })
-
-  const logoStyle = computed(() => ({ width: `${props.size}px` }))
-</script>
-
 <style lang="scss" scoped>
-  .art-logo {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+.art-logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    img {
-      width: 100%;
-      height: 100%;
-    }
+  img {
+    width: 100%;
+    height: 100%;
   }
+}
 </style>
